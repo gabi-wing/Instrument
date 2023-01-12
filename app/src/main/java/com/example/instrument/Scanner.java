0f89bae720f9d9a880ed9649293e80ed993ed1af
Gabi Wing <gwing2004@gmail.com>
2023-01-12 11:28:27 -0600

    initial commit

---
+++ app/src/main/java/com/example/instrument/Scanner.java
@@ -0,0 +1,16 @@
+package com.example.instrument;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Scanner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+
+
+    }
+}
