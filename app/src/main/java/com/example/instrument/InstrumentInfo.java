452958774b2b1d84828bc6bb53a2bc47f0fceab9
Gabi Wing <gwing2004@gmail.com>
2023-01-27 19:01:33 -0600

    added on click handler for each element in instrument list and made collapsable group

--- app/src/main/java/com/example/instrument/InstrumentInfo.java
+++ app/src/main/java/com/example/instrument/InstrumentInfo.java
@@ -1,9 +1,14 @@
 package com.example.instrument;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Group;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
--- app/src/main/java/com/example/instrument/MainActivity.java
+++ app/src/main/java/com/example/instrument/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.instrument;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Group;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -27,6 +30,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        Group filterGroup = findViewById(R.id.filterGroup);
+        Button filterButton = findViewById(R.id.filterButton);
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(filterGroup.getVisibility()==View.GONE){
+                    filterGroup.setVisibility(View.VISIBLE);
+                }
+                else{
+                    filterGroup.setVisibility(View.GONE);
+                }
+            }
+        });
+
         ArrayList<Instrument> instruments = new ArrayList<>();
         instruments.add(new Instrument("Fl1","flute"));
         instruments.add(new Instrument("Fl2","flute"));
@@ -35,9 +53,22 @@ public class MainActivity extends AppCompatActivity {
         instruments.add(new Instrument("Tu1","tuba"));
         instruments.add(new Instrument("Tr1","trombone"));
 
+
         InstrumentAdapter adapter = new InstrumentAdapter(this, R.layout.single_intrument_item, instruments);
         ListView listView = (ListView) findViewById(R.id.instrumentList);
         listView.setAdapter(adapter);
+
+        //handler for clicking items in instrument list
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent singleItem = new Intent(getApplicationContext(), InstrumentInfo.class);
+                startActivity(singleItem);
+
+
+            }
+        });
+
     }
 
 }
